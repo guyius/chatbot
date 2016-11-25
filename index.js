b56034996a8746da84000306352d741955666d3e
guyius <guyb@wix.com>
2016-11-25 19:41:39 +0200

    removed secret token added dev end token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
-const token = "EAAEwe4Bpp4YBAL3ZBPUDOYduv3vLQ4k4a0kDHdptLQw1FIJjFqQjQPAOcadVTj5WBFySKrZBNKNVhne3cZC6ZAuHeI6SZAFdqK9aNhbiH8bQxIT7wGZAkmcZAOuJX36JUfTvikdrYQ3o01PiYnzCuemkOwz9zdZCCZCbk47TMgxrZBmwZDZD";
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
