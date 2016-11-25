f1347643ce80ce35547bce4ca65a6e23c627d14d
guyius <guyb@wix.com>
2016-11-25 19:39:36 +0200

    Added token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+const token = "EAAEwe4Bpp4YBAL3ZBPUDOYduv3vLQ4k4a0kDHdptLQw1FIJjFqQjQPAOcadVTj5WBFySKrZBNKNVhne3cZC6ZAuHeI6SZAFdqK9aNhbiH8bQxIT7wGZAkmcZAOuJX36JUfTvikdrYQ3o01PiYnzCuemkOwz9zdZCCZCbk47TMgxrZBmwZDZD";
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
